719bea347fa061d54c6b99ed9d302eea154cdef9
2017_anastasiyayan <2017_anastasiyayan@LAB27-5.school.npmg.org>
2022-03-09 13:02:09 +0200

    Initial Commit

---
+++ src/main/java/school/hub/DaskalnicApplication.java
@@ -0,0 +1,13 @@
+package school.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaskalnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaskalnicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/hub/DaskalnicApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaskalnicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
